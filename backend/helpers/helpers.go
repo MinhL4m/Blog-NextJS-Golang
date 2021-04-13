5a80ce61216c0a220bd3fab5c636bc987b60e42d
Minh Lam <ltm1701@gmail.com>
2021-04-13 15:06:02 -0600

    backend\helpers functions for hash and validations

---
+++ backend/helpers/helpers.go
@@ -0,0 +1,48 @@
+package helpers
+
+import (
+	"errors"
+
+	"github.com/badoux/checkmail"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+/**----------Validation Functions-------------*/
+func UpdateValid(username, password, email string) error {
+	if username == "" {
+		return errors.New("required username")
+	}
+	if password == "" {
+		return errors.New("required password")
+	}
+	if email == "" {
+		return errors.New("required email")
+	}
+	if err := checkmail.ValidateFormat(email); err != nil {
+		return errors.New("invalid email")
+	}
+
+	return nil
+}
+
+func LoginValid(password, email string) error {
+	if password == "" {
+		return errors.New("required password")
+	}
+	if email == "" {
+		return errors.New("required email")
+	}
+	if err := checkmail.ValidateFormat(email); err != nil {
+		return errors.New("invalid email")
+	}
+
+	return nil
+}
