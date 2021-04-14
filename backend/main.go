74ee0bc1159dec40fcc2dd4a109be0c54a7b8238
Minh Lam <ltm1701@gmail.com>
2021-04-13 18:33:23 -0600

    backend\change import and add more param

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	controller "github.com/MinhL4m/blogs/api/controller"
 	"github.com/joho/godotenv"
 )
 
@@ -14,11 +15,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	a := App{}
+	a := controller.App{}
 
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_HOST"),
+		os.Getenv("APP_DB_PORT"),
 		os.Getenv("APP_DB_NAME"),
 		os.Getenv("APP_DB_TYPE"),
 	)
